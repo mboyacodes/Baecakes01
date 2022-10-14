6b14dbdb3a3fa8db53ea41f1f9da2068d5295b6a
Felix Mboya <felixmboya.design@gmail.com>
2022-10-15 00:02:18 +0300

    Add files via upload

---
+++ Baecakes website/indexBC.js
@@ -0,0 +1,5 @@
+const email = document.getElementById('InputEmail1');
+const password = document.getElementById('InputPassword1');
+const form = document.getElementById('form');
+
+form. addEventListener('submit , (e) =>')
